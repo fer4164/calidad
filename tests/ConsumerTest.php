b6065d96f40784a5f525edfa8b3390b99abe6dfd
Dauun <dauun.said@gmail.com>
2018-11-21 20:51:17 -0600

    Update ConsumerTest.php

--- tests/ConsumerTest.php
+++ tests/ConsumerTest.php
@@ -11,14 +11,10 @@ class ConsumerTest extends \PHPUnit_Framework_TestCase
     */
     public function testNombreCompleto()
     {
-
-    }
-
-    /**
-    * Probar email válido
-    */
-    public function testEmail()
-    {
-
+        $consumer = new Dummy();
+        $consumer->setNombre('Said');
+        $consumer->setApellido('Dawn');
+        $result = $consumer->nombreCompleto();
+        $this->assertEquals('Said Dawn', $result);
     }
 }
